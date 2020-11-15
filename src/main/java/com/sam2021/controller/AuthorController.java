c29f957c2d105c341a2d7cb13d7c838f81bed5bb
Stephen Cook <sjc5897@rit.edu>
2020-11-15 15:53:32 -0500

    Author Refactor

--- src/main/java/com/sam2021/controller/AuthorController.java
+++ src/main/java/com/sam2021/controller/AuthorController.java
@@ -1,88 +1,121 @@
 package com.sam2021.controller;
 
 import com.sam2021.database.SubmissionEntity;
-import com.sam2021.database.UserEntity;
-import com.sam2021.services.AuthorService;
+import com.sam2021.services.AuthenitcationService;
 import com.sam2021.services.FileService;
+import com.sam2021.services.SubmissionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.List;
 
+/**
+ * This is a custom controller for Author pages into the system
+ * Language: Java 13
+ * Framework: Spring
+ * Author: Stephen Cook <sjc5897@rit.edu>, Malcolm Lambrecht
+ * Created: 10/26/20
+ * Last Edit: 11/15/20
+ */
 @Controller
 public class AuthorController {
+    // Services
     @Autowired
-    AuthorService service;
+    private SubmissionService submissionService;
 
     @Autowired
-    FileService fileService;
+    private FileService fileService;
 
+    @Autowired
+    private AuthenitcationService authenitcationService;
+
+    /**
+     * This handles gets to the url /author
+     * @param model     The object model for thymeleaf
+     * @param request   The http servlet request
+     * @return          String representing either redirect or author page
+     */
     @RequestMapping(value="/author", method= RequestMethod.GET)
-    public String getAuthorPage(@ModelAttribute("user") UserEntity user, Model model, HttpServletRequest request){
+    public String getAuthorPage(Model model, HttpServletRequest request){
 
+        // Authenticate request
         HttpSession session = request.getSession();
-        if(session.isNew()){
-            return "redirect:/login";
-        }
-        Long uid = (Long) session.getAttribute("uid");
-        String role = (String) session.getAttribute("role");
-        if(!role.equals("author")){
-            return "redirect:/" + role;
+        String ret = authenitcationService.auth(session,"author");
+        if(ret != null){
+            return ret;
         }
 
-        List<SubmissionEntity> submissions = service.getAuthorsSubmissions(uid);
+        // Get submissions
+        List<SubmissionEntity> submissions = submissionService.getAuthorsSubmissions((Long) session.getAttribute("uid"));
         if(submissions != null &&  submissions.size() > 1){
             model.addAttribute("submissions", submissions);
         }
+
+        // Return page
         return "author";
     }
 
-    @RequestMapping(value="/author",method= RequestMethod.POST)
-    public String handleAuthorSubmit(@ModelAttribute("user") UserEntity user, Model model){
-        model.addAttribute("user", user);
-        return "redirect:/author";
-    }
-
+    /**
+     * Handles get to url /author/new
+     * @param request   HttpServletRequest of the current request
+     * @return String representing the new submission form
+     */
     @RequestMapping(value="/author/new", method = RequestMethod.GET)
-    public String getSubmissionPage(@ModelAttribute("user") UserEntity user,Model model){
-        model.addAttribute("user", user);
+    public String getSubmissionPage(HttpServletRequest request){
+        // Authenticate request
+        HttpSession session = request.getSession();
+        String ret = authenitcationService.auth(session,"author");
+        if(ret != null){
+            return ret;
+        }
         return "new_sub";
     }
 
+    /**
+     * Handles Posts from the new submission form
+     * @param request   HttpServletRequest of the current request
+     * @param file      Multipart File representing file path
+     * @param email     String representing contact email address
+     * @param title     String representing title
+     * @param format    String representing the file format
+     * @param authorList String representing the authors
+     * @param version   String representing the version of the paper
+     * @return String representing redirect
+     */
     @RequestMapping(value="/author/new", method = RequestMethod.POST)
-    public String submit(RedirectAttributes redirectAttributes,
-                         HttpServletRequest request,
+    public String submit(HttpServletRequest request,
                          @RequestParam("file") MultipartFile file,
                          @RequestParam("email")String email,
                          @RequestParam("title")String title,
                          @RequestParam("format") String format,
                          @RequestParam("authorList") String authorList,
-                         @RequestParam(value = "version", required = false) String version,
-                         @ModelAttribute("user") UserEntity user,
-                         Model model){
+                         @RequestParam(value = "version", required = false) String version){
 
+        // Authenticate request
         HttpSession session = request.getSession();
-        if(session.isNew()){
-            return "redirect:/login";
-        }
-        Long uid = (Long) session.getAttribute("uid");
-        String role = (String) session.getAttribute("role");
-        if(!role.equals("author")){
-            return "redirect:/" + role;
+        String ret = authenitcationService.auth(session,"author");
+        if(ret != null){
+            return ret;
         }
 
+        //Create new submission
+        boolean suc = submissionService.addNewSubmission(email,title,file.getOriginalFilename(),
+                format,authorList,Integer.parseInt(version),
+                (Long) session.getAttribute("uid"));
+        // Upload file
+        if(suc){
+            fileService.uploadFile(file);
+        }
 
-        service.addNewSubmission(email,title,file.getOriginalFilename(),format,authorList,Integer.parseInt(version),uid);
-        fileService.uploadFile(file);
+        //Upload the file
+
         return "redirect:/author";
     }
 
--- src/main/java/com/sam2021/services/SubmissionService.java
+++ src/main/java/com/sam2021/services/SubmissionService.java
@@ -7,11 +7,59 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * Service for accessing the data of the submission repo
+ * Language: Java 13
+ * Framework: Spring
+ * Author: Stephen Cook <sjc5897@rit.edu>, Malcolm Lambrecht
+ * Created: 10/26/20
+ * Last Edit: 11/15/20
+ */
 @Service
 public class SubmissionService {
+    // Wire Repo
     @Autowired
     SubmissionRepo submissionRepo;
-    public List<SubmissionEntity> getSubmission(Long id){
-        return submissionRepo.findById(id);
+
+    /**
+     * Gets a submission by an id
+     * @param id the id of the submission
+     * @return  a Submission Entity
+     */
+    public SubmissionEntity getSubmission(Long id){
+        return submissionRepo.findOneById(id);
+    }
+
+    /**
+     * Gets all the submissions by author id
+     * @param id    author id
+     * @return      List of return submissions
+     */
+    public List<SubmissionEntity> getAuthorsSubmissions(Long id){
+        try{
+            return submissionRepo.getAllByAuthorId(id);
+        }catch (Exception ex){
+            return null;
+        }
+    }
+
+    /**
+     * Adds new submission to the database
+     * @param email         String representing contact email address
+     * @param title         String representing title
+     * @param file_name     String File representing file path
+     * @param format        String representing the file format
+     * @param author_list   String representing the authors
+     * @param version       String representing the version of the paper
+     * @param author_id     Long representing the author
+     * @return              Boolean representing success
+     */
+    public boolean addNewSubmission(String email, String title, String file_name, String format,String author_list,int version, Long author_id){
+        try {
+            submissionRepo.save(new SubmissionEntity(email,title,file_name,format,author_list,version,author_id,"SUBMITTED"));
+            return true;
+        }catch (Exception e){
+            return false;
+        }
     }
 }
